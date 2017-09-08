c3f3ac4ecea5c1bc32bdce25147101821a427dc3
Ahmed Rebai <ahmed.bouhmid94@gmail.com>
2017-09-08 10:22:24 +0100

    resolving other problems

--- strings/Find_a_string.py
+++ strings/Find_a_string.py
@@ -2,7 +2,8 @@ def count_substring(string, sub_string):
     count=0
     while sub_string in string:
       count=count+1
-      string=string[string.find(sub_string)+1:]
+      string=string[string.find(sub_string):]
+      print("string is" + string)
       return count
 
 
@@ -11,6 +12,7 @@ def count_substring(string, sub_string):
 if __name__ == '__main__':
     string = input().strip()
     sub_string = input().strip()
+    print("sub syring" + sub_string)
 
     count = count_substring(string, sub_string)
     print(count)
